252fd67424fc6b9cb0f2477ac5be4bca29bd3608
nonthasak laoluerat <nonthasak.l@Appels-iMac.local>
2023-07-11 22:44:39 +0700

    stash: login

---
+++ src/pages/oauth/callback.tsx
@@ -0,0 +1,13 @@
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+import { useEffect } from "react";
+import { useRouter } from "next/router";
+
+const Callback = () => {
+  return (
+    <>
+      <div>authenticating</div>
+    </>
+  );
+};
+
+export default Callback;
