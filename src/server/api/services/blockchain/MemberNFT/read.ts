e59e6cfaf1cfc0162bdac42632d12a4bbb84d660
nonthasak laoluerat <nonthasak.l@Appels-iMac.local>
2023-08-25 17:50:48 +0700

    upd: update contract data to mainnet

--- src/server/api/services/blockchain/MemberNFT/read.ts
+++ src/server/api/services/blockchain/MemberNFT/read.ts
@@ -6,11 +6,11 @@ import {
   abi as tAbi,
   address as tAddress,
 } from "~/blockchain/MemberNFT/testnet";
-import { testnetClient } from "../provider";
+import { publicClient } from "../provider";
 import { checkAdminPeriod } from "~/utils/checkAdminPeriod";
 
 export async function getStateOf(wallet: `0x${string}`) {
-  const data = await testnetClient.readContract({
+  const data = await publicClient.readContract({
     address:
       process.env.NEXT_PUBLIC_network === "Testnet" ? tAddress : mAddress,
     abi: process.env.NEXT_PUBLIC_network === "Testnet" ? tAbi : mAbi,
--- src/server/api/services/blockchain/provider.ts
+++ src/server/api/services/blockchain/provider.ts
@@ -2,14 +2,14 @@ import { createPublicClient, http } from "viem";
 import { bitkubMainnet, bitkubTestnet } from "~/blockchain/network";
 
 export const publicClient = createPublicClient({
-  chain: bitkubMainnet,
-  // process.env.NEXT_PUBLIC_network === "Testnet"
-  //   ? bitkubTestnet
-  //   : bitkubMainnet,
+  chain:
+    process.env.NEXT_PUBLIC_network === "Testnet"
+      ? bitkubTestnet
+      : bitkubMainnet,
   transport: http(),
 });
 
-export const testnetClient = createPublicClient({
-  chain: bitkubTestnet,
-  transport: http(),
-});
+// export const testnetClient = createPublicClient({
+//   chain: bitkubTestnet,
+//   transport: http(),
+// });
