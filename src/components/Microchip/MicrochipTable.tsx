17ece24b507b9033792b4e62d6aa3cdeeaaef8b4
nonthasak laoluerat <nonthasak.l@Appels-iMac.local>
2023-11-07 23:24:34 +0700

    fix: cannot buy microchip when microchip out of stock

--- src/components/Microchip/MicrochipTable.tsx
+++ src/components/Microchip/MicrochipTable.tsx
@@ -7,6 +7,7 @@ import { toast } from "react-toastify";
 
 const MicrochipTable = () => {
   const { wallet } = useBitkubNext();
+
   const {
     data: microchips,
     isLoading,
--- src/pages/microchip/index.tsx
+++ src/pages/microchip/index.tsx
@@ -5,10 +5,13 @@ import { Layout } from "~/components/Shared/Layout";
 import { BiSolidMicrochip } from "react-icons/bi";
 import { useBitkubNext } from "~/contexts/bitkubNextContext";
 import { useRouter } from "next/router";
+import { api } from "~/utils/api";
 
 const Microchip = () => {
   const { replace } = useRouter();
   const { isConnected } = useBitkubNext();
+  const { data: inventory, isLoading: inventoryLoading } =
+    api.microchip.getAll.useQuery();
 
   useEffect(() => {
     if (!isConnected) {
@@ -21,9 +24,11 @@ const Microchip = () => {
       <div className="mt-[90px] h-[90vh] w-full">
         <div className="grid grid-cols-1 px-10">
           <div className="place-self-end">
-            <Link href="/microchip/buy" className="btn-primary btn-xs btn">
-              <BiSolidMicrochip /> buy microchip
-            </Link>
+            {inventory == undefined || inventory.count <= 0 ? null : (
+              <Link href="/microchip/buy" className="btn-primary btn-xs btn">
+                <BiSolidMicrochip /> buy microchip
+              </Link>
+            )}
           </div>
           <div className="overflow-auto">
             <MicrochipTable />
