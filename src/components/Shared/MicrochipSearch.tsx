e0b6ede750a277b255c23771bee4a4af08ac1ae7
Nonthasak Laoluerat <nonthasak.l@gmail.com>
2024-07-18 23:44:20 +0700

    upd: update search by microchip

--- src/components/Shared/MicrochipSearch.tsx
+++ src/components/Shared/MicrochipSearch.tsx
@@ -1,15 +1,30 @@
 import React from "react";
 import { ImSearch } from "react-icons/im";
 import { useRef } from "react";
+import { api } from "~/utils/api";
 
 function MicrochipSearch() {
   const searchInput = useRef<HTMLInputElement>(null);
 
+  const { data: allMetadata, isLoading } =
+    api.microchip.getAllMetadata.useQuery();
+
   function handleSearch() {
+    if (isLoading) return;
     const startedWithNumber = /^[0-9]/.test(searchInput.current!.value.trim());
 
     if (window !== undefined && startedWithNumber) {
-      window.open(`https://jaothui.com/cert/${searchInput.current!.value}`);
+      const index = allMetadata?.findIndex(
+        (m) => m.certify.microchip == searchInput.current!.value
+      );
+
+      console.log(index);
+
+      if (index === undefined) return;
+
+      window.open(
+        `https://jaothui.com/cert/${searchInput.current!.value}?i=${index + 1}`
+      );
     } else {
       window.open(
         `https://jaothui.com/cert/search?q=${searchInput.current!.value}`
@@ -26,7 +41,11 @@ function MicrochipSearch() {
         className="input input-sm w-full max-w-[420px] rounded-full text-black focus:outline-none"
         ref={searchInput}
       ></input>
-      <button className="text-slate-700" onClick={handleSearch}>
+      <button
+        disabled={isLoading}
+        className="text-slate-700"
+        onClick={handleSearch}
+      >
         <ImSearch size={30} />
       </button>
     </div>
